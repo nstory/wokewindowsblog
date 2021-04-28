62af2a5443bf1115252e58236fd1166e5a993ccc
nstory <nstory@users.noreply.github.com>
2021-04-28 13:51:46 +0000

    jekyll build from Action d3d36187263ce778d154a889cd0f3773f5a0579f

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "Boston Parking Tickets Data",
+        "excerpt":"We recently received a response to our public records request to the City of Boston’s Transportation Department, and it’s a pretty cool set of records: Boston parking tickets from 2018 to Aug 19, 2020. There are 3,200,977 tickets in this dataset! At the Woke Windows Project, our focus is on...","categories": [],
+        "tags": [],
+        "url": "https://blog.wokewindows.org/2020/09/17/parking-tickets.html",
+        "teaser": null
+      },{
+        "title": "Boston Public Records Log",
+        "excerpt":"EDIT Apr 14, 2021 Shawn Breen made a request for the Boston PRR log on MuckRock FOIA Request: FOIA logs for years 2016, 2017, 2018, 2019 and 2020. The data he received has the Public Record Desired field for all requests, which makes the data much more useful. The records...","categories": [],
+        "tags": [],
+        "url": "https://blog.wokewindows.org/2021/01/14/boston-public-records-log.html",
+        "teaser": null
+      },{
+        "title": "PRR: Boston Public Records Responses",
+        "excerpt":"Following up on my request for the Boston Public Records Log, I requested the responses to a number of public records requests. I’ve posted those responses below. The most interesting are problably: B000971-070120 — Boston Police Internal Affairs case log from 2010 to sometime in 2020 (though I don’t believe...","categories": [],
+        "tags": [],
+        "url": "https://blog.wokewindows.org/2021/03/26/prr-boston-public-records-responses.html",
+        "teaser": null
+      },{
+        "title": "Public Records Appeals Database",
+        "excerpt":"I just pushed live a new feature on Woke Windows: a searchable database of Massachusetts public records appeals. Using this database, you can search appeals made to the Secretary of the Commonwealth, including the text of determinations issued by the Commonwealth’s Supervisor of Records (while the Commonwealth does provide a...","categories": [],
+        "tags": [],
+        "url": "https://blog.wokewindows.org/2021/03/28/appeals-database.html",
+        "teaser": null
+      },{
+        "title": "Boston Traffic Citations 2011 - 2020",
+        "excerpt":"I just finished pushing updated traffic citation data to the Woke Windows database. Previously, Woke Windows just had the traffic tickets written in 2019. Now we have data spanning the years 2011 through 2020. These records have fields indicating: the Boston Police officer who wrote the ticket, the location where...","categories": [],
+        "tags": [],
+        "url": "https://blog.wokewindows.org/2021/04/24/boston-traffic-citations.html",
+        "teaser": null
+      },{
+        "title": "IAPro Documentation and Manuals",
+        "excerpt":"IAPro is a software package for managing internal affairs cases that is used by many police departments in Massachusetts, including the Boston Police Department, and the State Police. See the IAPro list of clients; they have hundreds around the world. I am particularly interested in collecting, through public records requests,...","categories": [],
+        "tags": [],
+        "url": "https://blog.wokewindows.org/2021/04/28/iapro-docs.html",
+        "teaser": null
+      }]
